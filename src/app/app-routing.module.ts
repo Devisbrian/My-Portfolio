bdd7551871a0047bcf03191990e12ba68b7217b1
Devisbrian <otienonduko@gmail.com>
2023-09-12 19:50:04 +0300

    added service info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
 
 const routes: Routes = [
   {path:'contact',component:ContactComponent},
   {path:'home',component:HomeComponent},
-  {path:'portfolio',component:HomeComponent}
+  {path:'portfolio',component:HomeComponent},
+  {path:'service',component:ServiceComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { ServiceComponent } from './service/service.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { IonicModule } from '@ionic/angular';
+import { ServiceComponent } from './service/service.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    ServiceComponent,
     PortfolioComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ServiceComponent,
+  
   ],
   imports: [
     BrowserModule,
