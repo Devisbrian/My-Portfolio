f62e3a814557e226cd51bcabaf276325a0270505
Devisbrian <otienonduko@gmail.com>
2023-09-12 21:17:53 +0300

    changed portfolio component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ServiceComponent } from './service/service.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   {path:'contact',component:ContactComponent},
   {path:'home',component:HomeComponent},
-  {path:'portfolio',component:HomeComponent},
+  {path:'portfolio',component:PortfolioComponent},
   {path:'service',component:ServiceComponent}
 
 ];
